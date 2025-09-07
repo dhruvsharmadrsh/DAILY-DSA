95684f403d122e9173379f2431d9a12ca8b6d4f1
Dhruv Sharma <dhruvsharmadrsh@gmail.com>
2025-09-07 18:37:52 +0530

    update

--- Array/6/9/25/InsertionSort.cpp
+++ Array/6/9/25/InsertionSort.cpp
@@ -1,8 +1,8 @@
 #include<iostream>
 using namespace std;
 
-void InsertionSort(int *arr,int n){
-    for(int i=0;i<n;i++){
+void InsertionSort(int arr[],int n){
+    for(int i=1;i<n;i++){
         for(int j=i;j>0;j--){
             if(arr[j]<arr[j-1]){
                 swap(arr[j],arr[j-1]);
@@ -15,20 +15,21 @@ void InsertionSort(int *arr,int n){
 }
 
 void printArr(int *arr,int n){
-    for(int i=0;i<n;i++){
-        cout<<arr[i]<<" ";
-    }
+    for(int i=0;i<n;i++)
+    cout<<arr[i]<<" ";
 }
-
 int main(){
+
+    int arr[1000];
     int n;
-    cout<<"enter the size of arr: ";
+    cout<<"enter the size of array";
     cin>>n;
-    int arr[10000];
-    cout<<"enter the the arr ele: ";
+
+    cout<<"enter the ele in array";
     for(int i=0;i<n;i++){
         cin>>arr[i];
     }
+
     InsertionSort(arr,n);
     printArr(arr,n);
     return 0;
